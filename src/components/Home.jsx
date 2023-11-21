b771aa5577365598b50daac8492994a8943dad22
OluwabukolaU <oluwabukola.umaru@gmail.com>
2023-11-21 19:24:30 +0100

    project update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,24 +1,26 @@
-import React from 'react';
-import thecodEmpress from '../assets/thecodEmpress.png';
-import {MdOutlineKeyboardArrowRight} from 'react-icons/md';
+import React from "react";
+import sophia from "../assets/sophia.png";
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 import { Link } from "react-scroll";
 const Home = () => {
   return (
     <div
-    name="home"
-    className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800"
-  >
-    <div className=" md:max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
-      <div className="flex flex-col justify-center h-full">
-        <h2 className="text-4xl sm:text-7xl font-bold text-white">
-          I'm a Software Developer
-        </h2>
-        <p className="text-gray-500 py-2 max-w-md">
-        Hello, and thank you for visiting my portfolio. I'm a dedicated 
-        and passionate tech enthusiast who has honed my skills over the past year, 
-        gaining experience in HTML, CSS, Javascript, React, Python,  Django, and Redux. I'm driven by 
-        a love for solving real-world problems through code.
-        </p>
+      name="home"
+      className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800"
+    >
+      <div className=" md:max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
+        <div className="flex flex-col justify-center h-full">
+          <h2 className="text-4xl sm:text-7xl font-bold text-white">
+            I'm a Software Developer
+          </h2>
+
+          <p className="text-gray-100 py-2 max-w-md">
+            Hello, and thank you for visiting my portfolio. I'm a dedicated and
+            passionate tech enthusiast who has honed my skills over the past
+            year, gaining experience in HTML, CSS, Javascript, React, Python,
+            Django, and Redux. I'm driven by a love for solving real-world
+            problems through code.
+          </p>
 
           <div>
             <Link
@@ -36,8 +38,8 @@ const Home = () => {
         </div>
 
         <div className="hidden md:flex">
-          <img 
-            src={thecodEmpress}
+          <img
+            src={sophia}
             alt="my profile"
             className="rounded-2xl mx-auto w-2/3 md:w-full"
           />
