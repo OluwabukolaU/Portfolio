90c863112400ccef4f4b676ae9e3dee9f0dcd97e
OluwabukolaU <oluwabukola.umaru@gmail.com>
2023-12-03 15:54:46 +0100

    more commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -16,7 +16,7 @@ const Contact = () => {
 
         <div className=" flex justify-center items-center">
           <form
-            action="https://getform.io/f/61c99527-2b15-42cf-9b55-ad37d2f7daa6"
+            action="https://getform.io/f/1d5d2e74-994c-46cb-a691-03affee79b0c"
             method="POST"
             className=" flex flex-col w-full md:w-1/2"
           >
