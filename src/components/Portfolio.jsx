0869ffe954d77cc429ab9d95a34ac902ae828b27
OluwabukolaU <oluwabukola.umaru@gmail.com>
2023-12-06 01:02:59 +0100

    portfolio update

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+/*import React from "react";
 import marketplace from "../assets/portfolio/marketplace.png";
 import travelwebsite from "../assets/portfolio/travelwebsite.png";
 import landingpage from "../assets/portfolio/landingpage.png";
@@ -47,6 +47,82 @@ const Portfolio = () => {
                   Demo
                 </button>
                 <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                  Code
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Portfolio;*/
+
+import React from "react";
+import marketplace from "../assets/portfolio/marketplace.png";
+import travelwebsite from "../assets/portfolio/travelwebsite.png";
+import landingpage from "../assets/portfolio/landingpage.png";
+
+
+const Portfolio = () => {
+  const portfolios = [
+    {
+      id: 1,
+      src: landingpage,
+      demoUrl: "https://landing-page-portfolio.netlify.app/",
+      codeUrl: "https://github.com/yourusername/landingpage"
+    },
+    {
+      id: 2,
+      src: travelwebsite,
+      demoUrl: "https://seasidess.netlify.app/",
+      codeUrl: "https://github.com/OluwabukolaU/Seasides"
+    },
+    {
+      id: 3,
+      src: marketplace,
+      demoUrl: "https://landing-page-portfolio.netlify.app/",
+      codeUrl: "https://github.com/yourusername/landingpage"
+    },
+   
+  ];
+
+  const openDemo = (url) => {
+    window.open(url, "_blank");
+  };
+
+  const openCode = (url) => {
+    window.open(url, "_blank");
+  };
+
+  return (
+    <div
+      name="portfolio"
+      className="bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen "
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-4">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500 ">
+            Portfolio
+          </p>
+          <p className="py-6">Check out some of my work right here</p>
+        </div>
+
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
+          {portfolios.map(({ id, src , demoUrl, codeUrl}) => (
+            <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
+              <img
+                src={src}
+                alt=""
+                className="rounded-md duration-200 hover:scale-105"
+              />
+              <div className="flex items-center justify-center">
+                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105" onClick={() =>openDemo(demoUrl)}>
+                  Demo
+                </button>
+                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105" onClick={() => openCode(codeUrl)}>
                   Code
                 </button>
               </div>
